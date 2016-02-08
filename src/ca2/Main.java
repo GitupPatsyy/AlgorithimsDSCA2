2aaf7db1ae308b3de18450ce5023e4f34b00e441
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-02-08 20:38:01 +0000

    Generate payslip method which demos polymorphism. Still some issues with the output

--- src/ca2/Main.java
+++ src/ca2/Main.java
@@ -7,12 +7,14 @@ package ca2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import static java.lang.Integer.parseInt;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -31,7 +33,7 @@ public class Main {
 
         do {
             System.out.println("1 - Create entry from an input file");
-            System.out.println("2 - Generate Staff P45");
+            System.out.println("2 - Generate Payslisps");
             System.out.println("3 - View all Drivers");
             System.out.println("4 - Display ALL Staff members");
             System.out.println("0 - Exit");
@@ -50,7 +52,8 @@ public class Main {
                 }
 
                 case 2: {//No idea what this will do yet.. Output file? 
-                    System.out.println("Generating staff P45...");
+                    System.out.println("Generating staff payslisps...");
+                    generatePayslips(model);
                     break;
                 }
 
@@ -62,80 +65,69 @@ public class Main {
             }
         } while (menu != 0);
     }
-        
-        //Read input file
- public static void readStaffInput(Scanner file, Model m) {
-            //File name required for input
-            System.out.println("Enter the file name for input:");
-            String inputFileName = file.nextLine();
-            File inputFile = new File(inputFileName);
-            try {
-                Scanner in = new Scanner(inputFile);
-                while(in.hasNextLine())
+
+    //Read input file
+    public static void readStaffInput(Scanner file, Model m) {
+        //File name required for input
+        System.out.println("Enter the file name for input:");
+        String inputFileName = file.nextLine();
+        File inputFile = new File(inputFileName);
+        try { //Try whatever is in the {}
+            Scanner in = new Scanner(inputFile); //read the file input name
+            while (in.hasNextLine()) //While the next line has...
+            {
+                String line = in.nextLine();
+                if (line.equalsIgnoreCase("D")) // if the next line as a D run the create driver method
                 {
-                    String line = in.nextLine();
-                    if(line.equalsIgnoreCase("D"))
-                    {
-                        createDriver(in, m);
-                        
-                    }
-                    
-                    
-                    else {
-                       System.out.println("Dum dum");
-                   }
+                    createDriver(in, m);
+
+                } else if (line.equalsIgnoreCase("M")) { // If it has an M or anything else there will be a mechanic added
+                    System.out.println("Dum dum");
                 }
-                in.close();
-                
             }
-            catch (FileNotFoundException ex){
-                System.out.println("None existent file baffoon\n");
-            }
+            in.close();
+
+        } catch (FileNotFoundException ex) { //File not found exception
+            System.out.println("None existent file baffoon\n"); // Print out a message to let the user know that there is no file with that name
         }
+    }
 
     private static void createDriver(Scanner in, Model m) {
-        
+
         String fn = in.nextLine(); //String for First name
         String ln = in.nextLine(); //String for Last name
         String em = in.nextLine(); //String for Email
-        System.out.println("Email " + em);
         int cn = in.nextInt();
         String blank = in.nextLine();//Read the /n left by the nextInt()
         String l = in.nextLine(); //String for License Number       
-        DateFormat df = new SimpleDateFormat ("yyyy-MM-dd"); //Date formatting
-        Date dateStart = null; 
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd"); //Date formatting
+        Date dateStart = null;
         String startDate = in.next();
         System.out.println(); //Reads the \n after the date field
         double hd = in.nextDouble(); //For hours drove
-        blank = in.nextLine(); //Eat up end of line
-        
-        
+
 //        System.out.print(fn +"--"+ ln +"--"+ em +"--"+ cn +"--"+ l +"--"+ dateStart +"--"+ hd);
-        
-        
-        try {
-            
-            dateStart = df.parse(startDate);
+        try { // Try to parse the date 
+
+            dateStart = df.parse(startDate); //Nww variable to hold the parse of the date
+        } catch (ParseException e) { //Catch clause
         }
-        catch(ParseException e) {
-            Logger.getLogger(em, startDate);
-        }
-        
-        Driver d = new Driver(fn, ln, em, cn, l, dateStart, hd);
-        m.addDriver(d);
-        }
- 
- //Method to view the arraylist
-   private static void viewDrivers(Model ml) {
-       List<Driver> drivers = ml.getDrivers();
-       System.out.println(); //Print a blank line for space
-        if(drivers.isEmpty()) { //If the drivers is empty print out the error below
+
+        Driver d = new Driver(fn, ln, em, cn, l, dateStart, hd); //New driver object
+        m.addDriver(d); //Use the models add driver method to add the driver to the arraylist
+    }
+
+    //Method to view the arraylist
+    private static void viewDrivers(Model ml) {
+        List<Driver> drivers = ml.getDrivers();
+        System.out.println(); //Print a blank line for space
+        if (drivers.isEmpty()) { //If the drivers is empty print out the error below
             System.out.println("There are no drivers within our database");
         } else {
-          System.out.printf("%10s %20s %20s %30s %12s %20s %10s %5s\n",//Formatting lenghts
-                    "StaffId", "First Name", "Last Name", "Email", "Contact No", "License No", "Start Date", "Hours Drove"); 
+            System.out.printf("%10s %20s %40s %40s %12s %25s %10s %5s\n",//Formatting lenghts
+                    "StaffId", "First Name", "Last Name", "Email", "Contact No", "License No", "Start Date", "Hours Drove");
             for (Driver dr : drivers) {
-                System.out.printf("%9s %20s %20s %20s %12s %20s%.2s %7s\n",
+                System.out.printf("%10s %20s %40s %40s %12s %25s %10s %5s\n",
                         dr.getStaffID(),
                         dr.getFirstName(),
                         dr.getLastName(),
@@ -147,19 +139,40 @@ public class Main {
             }
         }
         System.out.println();//Print a blank line for spacing
-    }  
-        
-        
-   
-    
-    
-    
+    }
+
+    private static void generatePayslips(Model m) {
+        //OBject for a STAFF memeber. 
+        //Staff member can be either driver or mechancic
+        Staff s = null;
+        //Read ids from the text file from the model
+        File inputFile = new File("payslips.txt");
+        try {
+            Scanner in = new Scanner(inputFile);
+            while (in.hasNextLine()) {
+                String line = in.nextLine(); //Eat up blank space
+                String id = in.nextLine();
+                int staffId = parseInt(id.trim());
+                System.out.println("Line " + line);
+                System.out.println("ID " + staffId);
+                if (line.equalsIgnoreCase("D")) {
+                    s = m.findDriverByID(staffId);
+                }
+                else if (line.equalsIgnoreCase("M")) {
+//                    s = m.findMechanicByID(staffId);
+                }
+                
+                if (s != null)
+                    //utilises polymorphism, can call drivers or the mechanics method to print payslips
+                    s.printPayslip();
+                else 
+                    System.out.println("Staff number: " + staffId + " non existent");
+
+            }
+        } catch (FileNotFoundException ex) {
+           System.out.println("No such file be in existence inside of this dimension - check another portal");
+        }
+    }
+
 }//Main method closing brace
-    
-        
-
-
-
-    
-
 
