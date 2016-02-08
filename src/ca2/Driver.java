f58c173a91c37c61745606ba0de69f4a47f20c04
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-02-08 20:35:42 +0000

    Added the printPayslip method and the viewStaff method

--- src/ca2/Driver.java
+++ src/ca2/Driver.java
@@ -5,7 +5,9 @@
  */
 package ca2;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  *
@@ -72,5 +74,26 @@ public class Driver extends Staff {
     public void setHoursDrove(double hoursDrove) {
         this.hoursDrove = hoursDrove;
     }
+    
+    public void viewStaff() {
+        System.out.printf("%5s %20s %30s %30s %30s %20s %15s %10s \n",
+                "Id", "First Name", "Last Name", "Email", "Contact No", "License No", "Start Date", "Hours Drove");
+        //Super class view method called
+        super.viewStaff();
+        System.out.printf("%10s 10d\n", this.startDate, this.hoursDrove);
+    }
+    
+      @Override
+    public void printPayslip()
+    {
+       this.viewStaff();
+       
+      Calendar mCalendar = Calendar.getInstance();    
+      String month = mCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+       
+        System.out.println("MONTHLY PAY FOR : " + month + this.hoursDrove*7);
+               
+    }
+
 
 }
