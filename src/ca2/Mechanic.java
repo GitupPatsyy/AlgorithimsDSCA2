5f979216518856829e1d4e32430b190d038b5015
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-02 10:56:18 +0000

    Mechanic Method changed, formatting changed too.

--- src/ca2/Mechanic.java
+++ src/ca2/Mechanic.java
@@ -74,33 +74,43 @@ public class Mechanic extends Staff {
     public void setOverTime(double overTime) {
         this.overTime = overTime;
     }
+    
+    //Formatting method for stafffview
+    public String formatStaffView() {
+        String str = String.format("%10s %20s %20s %20s %12s %15s %10s %10s\n",//Formatting lenghts
+                    "| StaffId |", "| First Name |", "| Last Name |", "| Email |", "| Contact No |", "| No of Services |", "| Start Date |", "| Over Time |");
+        
+        String staffInfo = super.getStaffInfo();
+        String dates = String.format("%15s 15s\n", this.startDate, this.overTime);
+        
+        return str + staffInfo + dates;
+        
+    }
+    
 
     //A display method to override the display from the superlass (Staff)
     //Display method is in both super classes sub classes, so polymorphism can be utilised here
     public void viewStaff() {
-
-        System.out.printf("%10s %20s %20s %20s %12s %15s %10s %10s\n",//Formatting lenghts
-                    "| StaffId |", "| First Name |", "| Last Name |", "| Email |", "| Contact No |", "| No of Services |", "| Start Date |", "| Over Time |");
-        //Super class view method called
-        super.viewStaff();
-        System.out.printf("%15s 15s\n", this.startDate, this.overTime);
-
+        System.out.println(this.formatStaffView());
     }
 
     @Override
-    public void printReview() {
-        this.viewStaff();
+    public String printReview() {
+       String staffView = this.formatStaffView();
 
         Calendar cCalendar = Calendar.getInstance();
         String month = cCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
         
-        System.out.println("Monthly OT hours for " + month + " are " + getOverTime());
+        String dateHeading = "Monthly OT hours for " + month + " are " + getOverTime();
+        String review;
         if (getOverTime() > 20){
-            System.out.println("Your work level has been above par and we would like to congratulate and hope you keep up the hard work");
+            review = "Your work level has been above par and we would like to congratulate and hope you keep up the hard work";
         }
         else {
-            System.out.println("Your work level is slacking you are going to have to the finger out or you may face a serious sacking.");
+             review = "Your work level is slacking you are going to have to the finger out or you may face a serious sacking.";
         }
+        
+        return staffView + dateHeading + review;
 
     }
 
